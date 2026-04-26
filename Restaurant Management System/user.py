1aba6e952f89e13865a44cd22046c8c2872eada6
TashdidDiganta <tashdiddiganta@gmail.com>
2026-04-26 23:29:58 +0600

    Resturent project order cart done

--- Restaurant Management System/user.py
+++ Restaurant Management System/user.py
@@ -1,17 +1,67 @@
 
-
 from abc import ABC
 
-class User():
-    def __init__(self,name,phone,email,address):
-        self.name = name 
-        self.email = email 
+
+class User(ABC):
+    def __init__(self, name, phone, email, address):
+        self.name = name
+        self.email = email
         self.address = address
         self.phone = phone
 
+
+class Customer(User):
+    def __init__(self, name, phone, email, address):
+        super().__init__(name, phone, email, address)
+        self.cart = Order()
+
+    def view_menu(self, restaurant):
+        restaurant.menu.show_menu()
+
+    def add_to_cart(self, restaurant, item_name, quantity):
+        item = restaurant.menu.find_item(item_name)
+        if item:
+            item.quantity = quantity
+            self.cart.add_item(item)
+            print("Item added")
+        else:
+            print("Item not found")
+
+    def view_cart(self):
+        print("**View Cart**")
+        print("Name\tPrice\tQuantity")
+        for item, quantity in self.cart.items.item():
+            print(f"{item.name} {item.price}{quantity}")
+        
+        print("Total Price : {self.cart.total_price}")
+
+
+class Order:
+    def __init__(self) -> None:
+        self.items = {}
+
+    def add_item(self,item):
+        if item in self.items:
+            self.items[item] += item.quantity
+        else:
+            self.items[item] = item.quantity
+
+    def remove(self, item):
+        if item in self.items:
+            del self.items[item]
+
+
+    def total_price(self):
+        return sum(item.price * quantity for item, quantity in self.items.items())  
+
+    def clear(self):
+        self.items = {}    
+
+
+
 class Employee(User):
-    def __init__(self, name, phone, email, address,age,designation,salary):
-        super().__init__(name, phone, email, address) 
+    def __init__(self, name, phone, email, address, age, designation, salary):
+        super().__init__(name, phone, email, address)
         self.age = age
         self.designation = designation
         self.salary = salary
@@ -19,20 +69,86 @@ class Employee(User):
 
 class Admin(User):
     def __init__(self, name, phone, email, address):
-        super().__init__(name, phone, email, address) 
-        self.employees = []
+        super().__init__(name, phone, email, address)
 
-    def add_employee(self, name, email, phone, address,age,designation,salary):
-        employee = Employee(name,email,phone,address,age,designation,salary)
+    def add_employee(self, restaurant, employee):
+        restaurant.add_employee(employee)
+
+    def view_employee(self, restaurant):
+        restaurant.view_employee()
+
+    def add_new_item(self, restaurant, item):
+        restaurant.menu.add_menu_item(item)
+
+    def delete_item(self, restaurant, item):
+        restaurant.menu.remove_item(item)
+
+
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+        self.employees = []
+        self.menu = Menu()
+
+    def add_employee(self, employee):
         self.employees.append(employee)
-        print(f"{name} is added!")
 
     def view_employee(self):
-        print(f"Employee List")
+        print("Employee List")
         for emp in self.employees:
-            print(emp.name, emp.email ,emp.phone, emp.address)
+            print(emp.name, emp.email, emp.phone, emp.address)
 
-add = Admin("karim", "345435", "karim@gmail.com", "Dhaka")
-add.add_employee("Sagor", "s@gmail.com", "3432543", "gaibandha", 45, "chef", 12343)
-add.view_employee()
 
+class Menu:
+    def __init__(self):
+        self.items = []
+
+    def add_menu_item(self, item):
+        self.items.append(item)
+
+    def find_item(self, item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+        return None
+
+    def remove_item(self, item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print("Item deleted")
+        else:
+            print("Item not found")
+
+    def show_menu(self):
+        print("*******Menu*********")
+        print("Name\tPrice\tQuantity")
+        for item in self.items:
+            print(f"{item.name}\t{item.price}\t{item.quantity}")
+
+
+class FoodItem:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+
+# Usage
+
+restaurent = Restaurant("Mama FastFood")
+
+mn = Menu()
+
+item = FoodItem("Pizza", 12.43,10)
+item2 = FoodItem("Burger", 10.43, 30)
+
+
+
+mn.add_menu_item(item)
+mn.add_menu_item(item2)
+mn.show_menu()
+
+
+customer = Customer("Diganta", 3245454,'adc@gmail.com', 'Gaibandha')
+customer.show_menu(restaurent)
